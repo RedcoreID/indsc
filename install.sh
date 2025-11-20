7c159294263ee92660592a2929a6f98fa0264082
Redcore ID <77957288+RedcoreID@users.noreply.github.com>
2025-11-21 06:07:39 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,19 +1,30 @@
 #!/bin/bash
 
-echo "[*] Menginstal IndSC (Termux)..."
+echo "[*] Menginstal IndSC..."
 
-PREFIX="$HOME/.local/bin"
-SHARE="$HOME/.local/share/indsc"
-mkdir -p "$PREFIX" "$SHARE"
-
-curl -sL "https://raw.githubusercontent.com/RedcoreID/indsc/main/engine.py" -o "$SHARE/engine.py"
-curl -sL "https://raw.githubusercontent.com/RedcoreID/indsc/main/run" -o "$PREFIX/indsc"
-
-chmod +x "$PREFIX/indsc"
-
-if ! grep -q "$PREFIX" <<< "$PATH"; then
-    echo "export PATH=\$PATH:$PREFIX" >> ~/.bashrc
+# Deteksi apakah Termux
+if [[ "$PREFIX" == *"com.termux"* ]]; then
+    echo "[*] Mode: Termux"
+    BIN="$HOME/.local/bin"
+    SHARE="$HOME/.local/share/indsc"
+    SUDO=""
+else
+    echo "[*] Mode: Linux"
+    BIN="/usr/local/bin"
+    SHARE="/usr/local/share/indsc"
+    SUDO="sudo"
 fi
 
+mkdir -p "$BIN" "$SHARE"
+
+echo "[*] Mengunduh file engine..."
+curl -sL "https://raw.githubusercontent.com/RedcoreID/indsc/main/engine.py" -o "$SHARE/engine.py"
+
+echo "[*] Mengunduh runner..."
+curl -sL "https://raw.githubusercontent.com/RedcoreID/indsc/main/run" -o "$BIN/indsc"
+
+chmod +x "$BIN/indsc"
+
 echo "[*] Instalasi selesai!"
-echo "Jalankan: indsc main.isc"
+echo "Jalankan dengan:"
+echo "    indsc file.isc"
